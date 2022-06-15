a24a9d9b95850ca59f5ee87915246aa9a19e130e
DESKTOP-ENCFMLR\hwmxy <hwmxycbc@outlook.com>
2022-06-15 22:49:05 +0800

    feat: 增加usb插拔事件监听 和 ipcMain dist:init事件监听

---
+++ src/main/logics/dist-change-logic.js
@@ -0,0 +1,49 @@
+const { usb, getDeviceList } = require('usb');
+const { existsSync } = require('fs');
+const event = require('../event');
+const { ipcMain } = require('electron');
+// u盘插拔事件
+const liseners = [];
+
+const files = ['C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'];
+
+function getExistDist() {
+  const list = [];
+
+  for (const dist of files) {
+    if (existsSync(`${dist}:\/`)) {
+      list.push({ name: dist });
+    } else {
+      break;
+    }
+  }
+
+  event.emit('DistChange', list);
+  return list;
+}
+
+function emitLisener() {
+  const info = getExistDist();
+  event.emit('postMessageToWindow', { key: 'dist:change', data: info });
+}
+
+usb.on('attach', emitLisener);
+
+usb.on('detach', emitLisener);
+
+function onDistChange(cb) {
+  if (typeof cb !== 'function') return;
+  if (!liseners.length) {
+    cb(getExistDist())
+  }
+  liseners.push(cb);
+}
+
+ipcMain.handle('dist:init', getExistDist)
+
+
+module.exports.onDistChange = onDistChange;
+
+module.exports.emitLisener = emitLisener;
+
+module.exports.getExistDist = getExistDist;
