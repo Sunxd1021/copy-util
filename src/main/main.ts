2e0cbf1c9c0f0d0152d9db0bf12065d118a012d9
DESKTOP-ENCFMLR\hwmxy <hwmxycbc@outlook.com>
2022-06-15 22:49:37 +0800

    feat: 主进程增加初始化复制逻辑

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,6 +14,7 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
+import { initCopyEvents } from './logics/copy-logic';
 
 export default class AppUpdater {
   constructor() {
@@ -106,6 +107,8 @@ const createWindow = async () => {
     shell.openExternal(edata.url);
     return { action: 'deny' };
   });
+
+  initCopyEvents(mainWindow);
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
